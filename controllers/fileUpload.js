f3dfe51f6dc0173e79eea6b8dd18201c20142081
NikshayMohla <78416149+NikshayMohla@users.noreply.github.com>
2024-07-24 18:30:30 +0530

    Image Upload Handler

--- controllers/fileUpload.js
+++ controllers/fileUpload.js
@@ -2,26 +2,19 @@ const File = require("../models/file");
 const cloudinary = require("cloudinary").v2;
 
 
-//localfileupload -> handler function
-
 exports.localFileUpload = async (req, res) => {
     try {
 
-        //fetch filefrom request
         const file = req.files.file;
         console.log("FILE AAGYI JEE -> ",file);
 
-
-        //create path where file need to be stored on server
         let path = __dirname + "/files/" + Date.now() + `.${file.name.split('.')[1]}`;
         console.log("PATH-> ", path)
 
-        //add path to the move fucntion
+      
         file.mv(path , (err) => {
             console.log(err);
         });
-
-        //create a successful response
         res.json({
             success:true,
             message:'Local File Uploaded Successfully',
@@ -50,17 +43,15 @@ async function uploadFileToCloudinary(file, folder, quality) {
     return await cloudinary.uploader.upload(file.tempFilePath, options);
 }
 
-//image upload ka hadnler
+
 exports.imageUpload = async (req, res) => {
     try{
-        //data fetch
         const { name, tags, email} = req.body;
         console.log(name,tags,email);
 
         const file = req.files.imageFile;
         console.log(file);
 
-        //Validation
         const supportedTypes = ["jpg", "jpeg", "png"];
         const fileType = file.name.split('.')[1].toLowerCase();
         console.log("File Type:", fileType);
@@ -72,12 +63,10 @@ exports.imageUpload = async (req, res) => {
             })
         }
 
-        //file format supported hai
         console.log("Uploading to Codehelp");
         const response = await uploadFileToCloudinary(file, "Codehelp");
         console.log(response);
 
-        //db me entry save krni h
         const fileData = await File.create({
             name,
             tags,
@@ -101,5 +90,4 @@ exports.imageUpload = async (req, res) => {
     }
 }
 
-//video upload ka handler
 
--- routes/FileUpload.js
+++ routes/FileUpload.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const {localFileUpload, imageUpload,videoUpload, imageSizeReducer} = require("../controllers/fileUpload");
 
-//api route
 router.post("/localFileUpload",localFileUpload );
 router.post("/imageUpload",imageUpload );
 // router.post("/videoUpload",videoUpload );
